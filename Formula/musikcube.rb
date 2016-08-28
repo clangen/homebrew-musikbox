0723893504d11ccee949cce6535576990d504e1a
Casey Langen <casey.langen@gmail.com>
2016-08-28 12:00:58 -0700

    Switched from MD5 to SHA256 for checksum.

--- Formula/musikcube.rb
+++ Formula/musikcube.rb
@@ -4,7 +4,7 @@ class Musikcube < Formula
     homepage 'http://musikcube.com'
     head 'git://github.com/clangen/musikcube.git'
     url 'https://github.com/clangen/musikcube/archive/0.1.1.tar.gz'
-    md5 '580bd10103cca722f057c7fb2ccd1619'
+    sha256 'a903e610094b4a22867585f16432007b6f6f2451a7894644290d6cee31889c84'
     version '0.1.1'
 
     depends_on 'cmake' => :build
