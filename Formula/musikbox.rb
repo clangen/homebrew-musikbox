99d41c2f10a5a5ac73ce40d726c295d558339664
Casey Langen <casey.langen@gmail.com>
2016-09-20 14:13:47 -0700

    Updated SHA hash for 0.2.1 after repo transfer.

--- Formula/musikbox.rb
+++ Formula/musikbox.rb
@@ -4,7 +4,7 @@ class Musikbox < Formula
     homepage 'http://musikcube.com'
     head 'git://github.com/clangen/musikcube.git'
     url 'https://github.com/clangen/musikcube/archive/0.2.1.tar.gz'
-    sha256 '2a7b1933f887d89c9dcb37321a44b21d39dffd3701bc054f97a34f2f6b68c3be'
+    sha256 '8bab1184ee2caec53327a45163c08950dfa11cf6bf949b6a9276f5ae54256b74'
     version '0.2.1'
 
     depends_on 'cmake' => :build
