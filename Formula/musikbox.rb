c7f9cf873409528679e5eae539451187854077c2
Casey Langen <casey.langen@gmail.com>
2016-12-04 12:54:34 -0800

    Upgraded to 0.5.0

--- Formula/musikbox.rb
+++ Formula/musikbox.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Musikbox < Formula
     homepage 'http://musikcube.com'
     head 'git://github.com/clangen/musikcube.git'
-    url 'https://github.com/clangen/musikcube/archive/0.4.1.tar.gz'
-    sha256 '86d851376f662003cea7799fba471bc13e8e28c5bc348bade0c8a540c45ff91e'
-    version '0.4.1'
+    url 'https://github.com/clangen/musikcube/archive/0.5.0.tar.gz'
+    sha256 'd213ec5bf45dfb180593a87e3113873d1fa47067e689bb7d5635ca73a74b3d21'
+    version '0.5.0'
 
     depends_on 'cmake' => :build
     depends_on 'boost'
