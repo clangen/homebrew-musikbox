1a0106526390d2034604bb95c05608a4c3ea7c13
Casey Langen <casey.langen@gmail.com>
2017-05-07 23:04:48 -0700

    Updated to 0.13.0.

--- Formula/musikbox.rb
+++ Formula/musikbox.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Musikbox < Formula
     homepage 'http://musikcube.com'
     head 'git://github.com/clangen/musikcube.git'
-    url 'https://github.com/clangen/musikcube/archive/0.12.1.tar.gz'
-    sha256 '24c79da265f538771d26cb9776df3c9ce57b39c18516b28f62414839ef7fb7fb'
-    version '0.12.1'
+    url 'https://github.com/clangen/musikcube/archive/0.13.0.tar.gz'
+    sha256 '49fcea177cb133c9bbec4096b7e9750ffc1841941bde1b2a63c9aee93ad255fa'
+    version '0.13.0'
 
     depends_on 'cmake' => :build
     depends_on 'boost'
@@ -14,6 +14,7 @@ class Musikbox < Formula
     depends_on 'flac'
     depends_on 'faad2'
     depends_on 'libmicrohttpd'
+    depends_on 'lame'
 
     def install
         system "cmake -DCMAKE_BUILD_TYPE=Release -DHOMEBREW_PREFIX=#{HOMEBREW_PREFIX} -DCMAKE_INSTALL_PREFIX=#{prefix} ."
